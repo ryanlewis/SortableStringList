1fdad5fd0e1d9eac06ba27120ccde9777020f7dc
Ryan Lewis <ryan.lewis@epiphanysolutions.co.uk>
2014-07-16 13:34:41 +0100

    First revision

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-sortablestringlist
-==================
+# Sortable String List
 
-Sortable string list property editor for Umbraco 7.x
+A property editor for Umbraco 7.x to provide a sortable list of strings. 
+
+## Screenshots
+### In action
+![example](example.png)
+
+### Configuration
+![config options](options.png)
+
+## Installation
+
+1. Clone the repository to `/App_Plugins/SortableStringList`
+2. A property value converter is available in [this gist](https://gist.github.com/ryanlewis/9eb27975d690b5f12e6c). This can be saved in your `/App_Code` directory
---
+++ sortablestringlist.js
@@ -0,0 +1,69 @@
+﻿var app = angular.module("umbraco");
+
+app.directive('wantFocus', function ($timeout) {
+  return {
+    link: function (scope, el, attrs) {
+      scope.$watch(attrs.wantFocus, function (value) {
+        if (value == attrs.index) {
+          $timeout(function () {
+            el[0].focus();
+          });
+        }
+      });
+    }
+  }
+});
+
+app.controller("EpiphanyStringListController", ['$scope', 'assetsService', function ($scope, assetsService) {
+
+  function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  $scope.focusedRow = -1;
+
+  $scope.sortableOptions = {
+    handle: '.handle'
+  }
+
+  if ($scope.model.value instanceof Array == false) {
+    $scope.model.value = [''];
+  }
+
+  $scope.min = isNumeric($scope.model.config.min) ? $scope.model.config.min : 0;
+  $scope.max = isNumeric($scope.model.config.max) && $scope.model.config.max !== 0 ? $scope.model.config.max : Number.MAX_VALUE;
+  $scope.allowBlanks = $scope.model.config.allowBlanks;
+
+  $scope.addRow = function () {
+    $scope.model.value.push('');
+  }
+
+  $scope.hasMinimum = function() {
+    return $scope.model.value.length == $scope.min;
+  }
+
+  $scope.hasMaximum = function() {
+    return $scope.model.value.length == $scope.max;
+  }
+
+  $scope.focusRow = function(index) {
+    $scope.focusedRow = index;
+  }
+
+  $scope.onKeydown = function (ev, index) {
+    // enter
+    var len = $scope.model.value.length;
+    if (ev.which == 13 && index + 1 == len && len < $scope.max) {
+      $scope.addRow();
+      $scope.focusRow($scope.model.value.length - 1);
+      ev.preventDefault();
+      return;
+    }
+  }
+
+  $scope.removeRow = function(index) {
+    $scope.model.value.splice(index, 1);
+  }
+
+  assetsService.loadCss('/App_Plugins/SortableStringList/sortablestringlist.css');
+}]);
